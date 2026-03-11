19933c67268e496ff5b2dae87a7715293b798e77
ANURAG TOMAR <anuragtomar19@ANURAGs-MacBook-Pro-3.local>
2026-03-11 16:31:47 +0530

    Added user model and login API with JWT authentication

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Please enter valid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      role: user.role
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ require("dotenv").config();
 
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+
+
 app.use(cors());
 app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Sab thik hai!");
 });
